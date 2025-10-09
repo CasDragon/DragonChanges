9e3a8a11ffa73a5523fac052be953c52e6acc747
CasDragon <krb16us@yahoo.com>
2025-10-09 12:50:20 -0400

    Buff Tricksters Stat Focus - Speed to +10

---
+++ DragonChanges/Content/TricksterStuff.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlueprintCore.Blueprints.CustomConfigurators.Classes;
+using BlueprintCore.Blueprints.References;
+using DragonLibrary.Utils;
+using Kingmaker.UnitLogic.FactLogic;
+
+namespace DragonChanges.Content
+{
+    internal class TricksterStuff
+    {
+        const string settingName = "statfocusspeedbuff";
+        const string settingDescription = "Buffs Stat Focus - Speed (Trickster feat) from a +1 bonus to speed to a +10.";
+        [DragonConfigure]
+        [DragonSetting(SettingCategories.Various, settingName, settingDescription)]
+        public static void PatchTricksterStatFocusSpeed()
+        {
+            if (SettingsAction.GetSetting<bool>(settingName))
+            {
+                Main.log.Log("Patching TricksterStatFocusSpeed to actually be useful.");
+                FeatureConfigurator.For(FeatureRefs.TricksterStatFocusSpeed)
+                    .EditComponent<AddStatBonus>(c => c.Value = 10)
+                    .Configure();
+            }
+        }
+    }
+}
