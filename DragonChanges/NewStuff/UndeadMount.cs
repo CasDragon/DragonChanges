263d41082c0c5c123dbe0bee14ffb829b4f4058e
CasDragon <krb16us@yahoo.com>
2026-01-07 10:14:11 -0500

    Undead mount code cleanup

--- DragonChanges/NewStuff/UndeadMount.cs
+++ DragonChanges/NewStuff/UndeadMount.cs
@@ -6,6 +6,7 @@ using DragonChanges.Utils;
 using DragonLibrary.Utils;
 using Kingmaker.Blueprints;
 using Kingmaker.Blueprints.Classes;
+using Kingmaker.Designers.Mechanics.Facts;
 using Kingmaker.EntitySystem.Stats;
 using Kingmaker.RuleSystem.Rules;
 using Kingmaker.UnitLogic.FactLogic;
@@ -77,110 +78,24 @@ namespace DragonChanges.NewStuff
         public static BlueprintUnit CreateUndeadMount()
         {
             Main.log.Log("Creating undead mount unit");
-            BlueprintUnit oghorse = TTTHelpers.CreateCopy<BlueprintUnit>(UnitRefs.AnimalCompanionUnitHorse.Reference.Get());
+            BlueprintUnit oghorse = UnitRefs.AnimalCompanionUnitHorse.Reference.Get();
+            BlueprintUnit phorse = UnitRefs.AnimalCompanionUnitHorseArmyGraveKnight_Large.Reference.Get();
             //BlueprintPortrait portrait = CreateUndeadMountPortrait();
-            SelectionEntry entry1 = new SelectionEntry();
-            entry1.IsParametrizedFeature = false;
-            entry1.IsFeatureSelectMythicSpellbook = false;
-            entry1.m_Selection = FeatureSelectionRefs.BasicFeatSelection.Reference.Get().ToReference<BlueprintFeatureSelectionReference>();
-            entry1.m_Features = [FeatureRefs.Dodge.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                FeatureRefs.Toughness.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                ParametrizedFeatureRefs.WeaponFocus.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                FeatureRefs.IronWill.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                FeatureRefs.CombatReflexes.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                FeatureRefs.CriticalFocus.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                ParametrizedFeatureRefs.ImprovedCritical.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                FeatureRefs.GreatFortitude.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                FeatureRefs.StaggeringCriticalFeature.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                FeatureRefs.Mobility.Reference.Get().ToReference<BlueprintFeatureReference>()];
-            entry1.ParamSpellSchool = Kingmaker.Blueprints.Classes.Spells.SpellSchool.None;
-            entry1.ParamWeaponCategory = Kingmaker.Enums.WeaponCategory.UnarmedStrike;
-            entry1.Stat = StatType.Unknown;
-            SelectionEntry entry2 = new SelectionEntry();
-            entry2.IsParametrizedFeature = true;
-            entry2.IsFeatureSelectMythicSpellbook = false;
-            entry2.m_Selection = FeatureSelectionRefs.BasicFeatSelection.Reference.Get().ToReference<BlueprintFeatureSelectionReference>();
-            entry2.m_Features = [FeatureRefs.Dodge.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                FeatureRefs.Toughness.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                ParametrizedFeatureRefs.WeaponFocus.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                FeatureRefs.IronWill.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                FeatureRefs.CombatReflexes.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                FeatureRefs.CriticalFocus.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                ParametrizedFeatureRefs.ImprovedCritical.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                FeatureRefs.GreatFortitude.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                FeatureRefs.PowerAttackFeature.Reference.Get().ToReference<BlueprintFeatureReference>()];
-            entry2.m_ParametrizedFeature = ParametrizedFeatureRefs.WeaponFocus.Reference.Get().ToReference<BlueprintParametrizedFeatureReference>();
-            entry2.ParamSpellSchool = Kingmaker.Blueprints.Classes.Spells.SpellSchool.None;
-            entry2.ParamWeaponCategory = Kingmaker.Enums.WeaponCategory.Hoof;
-            entry2.Stat = StatType.Unknown;
-            SelectionEntry entry3 = new SelectionEntry();
-            entry3.IsParametrizedFeature = true;
-            entry3.IsFeatureSelectMythicSpellbook = false;
-            entry3.m_Selection = FeatureSelectionRefs.BasicFeatSelection.Reference.Get().ToReference<BlueprintFeatureSelectionReference>();
-            entry3.m_Features = [FeatureRefs.Dodge.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                FeatureRefs.Toughness.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                ParametrizedFeatureRefs.WeaponFocus.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                FeatureRefs.IronWill.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                FeatureRefs.CombatReflexes.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                FeatureRefs.CriticalFocus.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                ParametrizedFeatureRefs.ImprovedCritical.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                FeatureRefs.GreatFortitude.Reference.Get().ToReference<BlueprintFeatureReference>(),
-                                FeatureRefs.PowerAttackFeature.Reference.Get().ToReference<BlueprintFeatureReference>()];
-            entry3.m_ParametrizedFeature = ParametrizedFeatureRefs.ImprovedCritical.Reference.Get().ToReference<BlueprintParametrizedFeatureReference>();
-            entry3.ParamSpellSchool = Kingmaker.Blueprints.Classes.Spells.SpellSchool.None;
-            entry3.ParamWeaponCategory = Kingmaker.Enums.WeaponCategory.UnarmedStrike;
-            entry3.Stat = StatType.Unknown;
-            ContextValue cmdvalue = new ContextValue();
-            cmdvalue.ValueType = ContextValueType.Simple;
-            cmdvalue.Value = 4;
-            cmdvalue.ValueRank = Kingmaker.Enums.AbilityRankType.Default;
-            cmdvalue.ValueShared = Kingmaker.UnitLogic.Abilities.AbilitySharedValue.Damage;
-            cmdvalue.Property = Kingmaker.UnitLogic.Mechanics.Properties.UnitProperty.None;
-            cmdvalue.PropertyName = Kingmaker.Enums.ContextPropertyName.Value1;
-            cmdvalue.m_AbilityParameter = AbilityParameterType.Level;
-            BlueprintUnit.UnitBody body = new BlueprintUnit.UnitBody();
-            body.DisableHands = false;
-            body.m_EmptyHandWeapon = ItemWeaponRefs.WeaponEmptyHand.Reference.Get().ToReference<BlueprintItemWeaponReference>();
-            body.m_PrimaryHand = ItemWeaponRefs.Bite1d4Large.Reference.Get().ToReference<BlueprintItemEquipmentHandReference>();
-            body.m_PrimaryHandAlternative1 = ItemWeaponRefs.Bite1d4Large.Reference.Get().ToReference<BlueprintItemEquipmentHandReference>();
-            body.m_PrimaryHandAlternative2 = ItemWeaponRefs.Bite1d4Large.Reference.Get().ToReference<BlueprintItemEquipmentHandReference>();
-            body.m_PrimaryHandAlternative3 = ItemWeaponRefs.Bite1d4Large.Reference.Get().ToReference<BlueprintItemEquipmentHandReference>();
-            body.ActiveHandSet = 0;
-            BlueprintUnit.UnitSkills skills = new BlueprintUnit.UnitSkills();
-            skills.Acrobatics = 0;
-            skills.Physique = 0;
-            skills.Diplomacy = 0;
-            skills.Thievery = 0;
-            skills.LoreNature = 0;
-            skills.Perception = 0;
-            skills.Stealth = 0;
-            skills.UseMagicDevice = 0;
-            skills.LoreReligion = 0;
-            skills.KnowledgeArcana = 0;
             return UnitConfigurator.New(UndeadUnit, Guids.UndeadMountUnit)
-                //.CopyFrom(oghorse)
-                .AddClassLevels(characterClass: CharacterClassRefs.AnimalCompanionClass.Reference.Get(),
-                    levels: 0,
-                    raceStat: StatType.Constitution,
-                    levelsStat: StatType.Strength,
-                    skills: [StatType.SkillPerception],
-                    selections: [entry1, entry2, entry3])
+                .CopyFrom(oghorse, typeof(AddClassLevels), typeof(CMDBonusAgainstManeuvers))
                 .AddAllowDyingCondition()
                 .AddResurrectOnRest()
                 .AddLockEquipmentSlot(slotType: LockEquipmentSlot.SlotType.MainHand)
                 .AddLockEquipmentSlot(slotType: LockEquipmentSlot.SlotType.OffHand)
-                .AddCMDBonusAgainstManeuvers(descriptor: Kingmaker.Enums.ModifierDescriptor.Racial,
-                    value: cmdvalue,
-                    maneuvers: [CombatManeuver.Trip, CombatManeuver.BullRush, CombatManeuver.Pull])
                 .AddFacts(facts: [FeatureRefs.HeadLocatorFeature.Reference.Get()])
-                .SetType(UnitTypeRefs.PlagueBeast_Horse.Reference.Get())
+                .SetType(phorse.Type)
                 .SetGender(Gender.Male)
                 .SetSize(Kingmaker.Enums.Size.Large)
                 .SetIsLeftHanded(false)
                 .SetAlignment(Kingmaker.Enums.Alignment.TrueNeutral)
                 .SetFaction(FactionRefs.Neutrals.Reference.Get())
                 .SetBrain("cf986dd7ba9d4ec46ad8a3a0406d02ae")
-                .SetPrefab("9b94fcd181ed5304e867b02c4faca9b8")
+                .SetPrefab(phorse.Prefab.AssetId)
                 .SetStrength(16)
                 .SetDexterity(13)
                 .SetIntelligence(2)
@@ -192,7 +107,7 @@ namespace DragonChanges.NewStuff
                 .SetMaxHP(0)
                 .SetIsCheater(false)
                 .SetIsFake(false)
-                .SetSkills(skills)
+                .SetSkills(oghorse.Skills)
                 .SetAddFacts([UnitFactRefs.NaturalArmor4.Reference.Get(),
                                 UnitFactRefs.ReducedReach.Reference.Get(),
                                 FeatureRefs.TripDefenseFourLegs.Reference.Get(),
@@ -202,7 +117,7 @@ namespace DragonChanges.NewStuff
                                 FeatureRefs.AnimalCompanionScent30.Reference.Get()])
                 .SetColor(new Color(0.15f, 0.15f, 0.15f, 1.0f))
                 .SetVisual(oghorse.Visual)
-                .SetBody(body)
+                .SetBody(oghorse.Body)
                 .SetFactionOverrides(new FactionOverrides())
                 .SetAlternativeBrains()
                 .SetAdditionalTemplates()
