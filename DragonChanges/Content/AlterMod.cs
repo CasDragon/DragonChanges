7153171d43b3abada2ec122d5b39bc6c761e6d10
CasDragon <krb16us@yahoo.com>
2026-06-08 12:42:35 -0400

    z

--- DragonChanges/Content/AlterMod.cs
+++ DragonChanges/Content/AlterMod.cs
@@ -65,85 +65,92 @@ namespace DragonChanges.Content
             {
                 if (ModCompat.microscopic)
                 {
-                    Main.log.Log("Patching various animal selections to include Nightmare horse (Microscopic)");
-                    string nightmare = "4bfe297b1d2c47d18aa770ec5c132194";
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionBase)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionDivineHound)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionDomain)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionDomainSeparatist)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionDruid)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionHunter)
-                        .AddToAllFeatures(nightmare )
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionMadDog)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionPrimalDruid)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionRanger)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionSacredHuntsmaster)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionSylvanSorcerer)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionUrbanHunter)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionWildlandShaman)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.CavalierMountSelection)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.BeastRiderMountSelection)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.ArcaneRiderMountSelection)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.BloodriderMountSelection)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.GhostRiderGhostMountSelection)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.NomadMountSelection)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.OrderOfThePawMountSelection)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.PaladinDivineMountSelection)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.SoheiMonasticMountHorseSelection)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    FeatureSelectionConfigurator.For(FeatureSelectionRefs.OracleRevelationBondedMount)
-                        .AddToAllFeatures(nightmare)
-                        .Configure();
-                    if (ModCompat.tttbase)
+                    try
                     {
-                        FeatureSelectionConfigurator.For("d9b99d9c48d2425894b565733e96c7e3")
+                        Main.log.Log("Patching various animal selections to include Nightmare horse (Microscopic)");
+                        string nightmare = "4bfe297b1d2c47d18aa770ec5c132194";
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionBase)
                             .AddToAllFeatures(nightmare)
                             .Configure();
-                        FeatureSelectionConfigurator.For("ecf97b544d584edb8bb0ba9e7de20751")
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionDivineHound)
                             .AddToAllFeatures(nightmare)
                             .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionDomain)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionDomainSeparatist)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionDruid)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionHunter)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionMadDog)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionPrimalDruid)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionRanger)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionSacredHuntsmaster)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionSylvanSorcerer)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionUrbanHunter)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.AnimalCompanionSelectionWildlandShaman)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.CavalierMountSelection)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.BeastRiderMountSelection)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.ArcaneRiderMountSelection)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.BloodriderMountSelection)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.GhostRiderGhostMountSelection)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.NomadMountSelection)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.OrderOfThePawMountSelection)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.PaladinDivineMountSelection)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.SoheiMonasticMountHorseSelection)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        FeatureSelectionConfigurator.For(FeatureSelectionRefs.OracleRevelationBondedMount)
+                            .AddToAllFeatures(nightmare)
+                            .Configure();
+                        if (ModCompat.tttbase)
+                        {
+                            FeatureSelectionConfigurator.For("d9b99d9c48d2425894b565733e96c7e3")
+                                .AddToAllFeatures(nightmare)
+                                .Configure();
+                            FeatureSelectionConfigurator.For("ecf97b544d584edb8bb0ba9e7de20751")
+                                .AddToAllFeatures(nightmare)
+                                .Configure();
+                        }
+                    }
+                    catch
+                    {
+                        Main.log.Log("Error adding Nightmare mount to selections");
                     }
                 }
                 else
