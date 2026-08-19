9c5e78991e03ae31b2a4ccdb31db48a58701d0e7
CasDragon <krb16us@yahoo.com>
2026-08-19 12:07:08 -0400

    Micro made me do this

--- DragonChanges/Content/MythicArcanist.cs
+++ DragonChanges/Content/MythicArcanist.cs
@@ -35,21 +35,24 @@ public class MythicArcanist
         }
     }
 
+    private static readonly BlueprintAbilityReference MageArmor2Spell = new() {deserializedGuid = BlueprintGuid.Parse("247d983c8ca04197bb2a1be55ce1d982")};
+    private static readonly BlueprintAbilityReference MageArmor3Spell = new() {deserializedGuid = BlueprintGuid.Parse("fdf0f15eab73419784f207ddcbdae4cf")};
+    private static readonly BlueprintAbilityReference MageArmor4Spell = new() {deserializedGuid = BlueprintGuid.Parse("3533d9e73bfa4d6e9bc44b5469d00f40")};
+
     private static void doThingy(AddAbilityUseTrigger component)
     {
         component.ForOneSpell = false;
         component.ForMultipleSpells = true;
         component.Abilities = [..component.Abilities, AbilityRefs.MageArmor.Reference.Get().ToReference<BlueprintAbilityReference>(),
-            DragonHelpers.MakeRef<BlueprintAbilityReference>("247d983c8ca04197bb2a1be55ce1d982"),
-            DragonHelpers.MakeRef<BlueprintAbilityReference>("fdf0f15eab73419784f207ddcbdae4cf"),
-            DragonHelpers.MakeRef<BlueprintAbilityReference>("3533d9e73bfa4d6e9bc44b5469d00f40")];
+            MageArmor2Spell, MageArmor3Spell, MageArmor4Spell];
     }
+
+    private static readonly BlueprintBuffReference MageArmor2Buff = new() {deserializedGuid = BlueprintGuid.Parse("f59bea4023804838bd5c86608dd40081")};
+    private static readonly BlueprintBuffReference MageArmor3Buff = new() {deserializedGuid = BlueprintGuid.Parse("e9c795aea6c741a7b3e425c52aa2674c")};
+    private static readonly BlueprintBuffReference MageArmor4Buff = new() {deserializedGuid = BlueprintGuid.Parse("9052af53d26e469cb2fe8f7111dfe7d9")};
 
     private static void doOther(SuppressBuffs component)
     {
-        component.m_Buffs = [.. component.m_Buffs, 
-            DragonHelpers.MakeRef<BlueprintBuffReference>("f59bea4023804838bd5c86608dd40081"),
-            DragonHelpers.MakeRef<BlueprintBuffReference>("e9c795aea6c741a7b3e425c52aa2674c"),
-            DragonHelpers.MakeRef<BlueprintBuffReference>("9052af53d26e469cb2fe8f7111dfe7d9")];
+        component.m_Buffs = [.. component.m_Buffs, MageArmor2Buff, MageArmor3Buff, MageArmor4Buff];
     }
 }
