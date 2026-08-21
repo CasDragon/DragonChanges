c988c72077e80c1cff8c98098bd788a5cc1bd976
CasDragon <krb16us@yahoo.com>
2026-08-21 09:38:36 -0400

    pp

---
+++ DragonChanges/Content/PrestigePlus.cs
@@ -0,0 +1,25 @@
+using BlueprintCore.Blueprints.CustomConfigurators.Classes.Spells;
+using BlueprintCore.Blueprints.References;
+using DragonLibrary.Utils;
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Classes.Spells;
+
+namespace DragonChanges.Content;
+
+public class PrestigePlus
+{
+    [DragonConfigure]
+    public static void BladeLash()
+    {
+        if (!ModCompat.pp)
+            return;
+        Main.log.Log("Adding Blade Lash to Warpriest/Inquist spell list");
+        BlueprintAbilityReference x = new() {deserializedGuid = BlueprintGuid.Parse("01346696ecff4f518bb21d9dca00041b")};
+        SpellListConfigurator.For(SpellListRefs.WarpriestSpelllist)
+            .AddToSpellsByLevel(new SpellLevelList(1) { m_Spells = [x] })
+            .Configure();
+        SpellListConfigurator.For(SpellListRefs.InquisitorSpellList)
+            .AddToSpellsByLevel(new SpellLevelList(1) { m_Spells = [x] })
+            .Configure();
+    }
+}
